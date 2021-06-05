fc5c0f8056d99e98fa58e5f1870268eb8b4fab89
Martynas Mickevičius <self@2m.lt>
2021-06-05 12:12:10 +0300

    Define API using Tapir

---
+++ api/src/test/scala/ApiSuite.scala
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2015 github.com/2m/citywasp-api/graphs/contributors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package lt.dvim.citywasp.api
+
+import java.nio.file.Files
+import java.nio.file.Paths
+
+import eu.timepit.refined.auto._
+import sttp.client3.testing.SttpBackendStub
+import sttp.model.Header
+import sttp.model.Uri
+import sttp.tapir.client.sttp.SttpClientInterpreter
+
+import lt.dvim.citywasp.api.Api
+import lt.dvim.citywasp.api.Model._
+
+class ApiSuite extends munit.FunSuite with DecodeResultOps {
+
+  val uri = Some(Uri.unsafeParse("test.lt"))
+  val params = Params.default
+  val expectedHeaders = Seq(
+    Header("Accept-Encoding", "gzip, deflate"),
+    Header("Accept", "application/json"),
+    Header("Accept-Language", "en-US"),
+    Header("app-version", "9.9.9"),
+    Header("country", "LT"),
+    Header("datetime", params.datetime.getEpochSecond().toString),
+    Header("deviceid", params.deviceId.toString),
+    Header("software", "0"),
+    Header("token", params.token),
+    Header("username", "")
+  )
+
+  test("Api.AvailableServices.Get - request") {
+    val request = SttpClientInterpreter.toRequest(Api.AvailableServices.Get, uri)
+    val prepared = request(params)
+    assertEquals(prepared.uri.toString, "test.lt/api/AvailableServices/Get")
+    assertEquals(prepared.headers, expectedHeaders)
+  }
+
+  test("Api.AvailableServices.Get - response") {
+    val testingBackend = SttpBackendStub.synchronous
+      .whenRequestMatches(_ => true)
+      .thenRespond(Files.readString(Paths.get(getClass.getResource("/services.json").toURI())))
+    val request = SttpClientInterpreter.toRequest(Api.AvailableServices.Get, uri)
+    val prepared = request(params)
+    val response = prepared.send(testingBackend)
+
+    val services = response.body.orThrow
+    assertEquals(services, List(Service(21, "service1", false), Service(19, "service2", false)))
+  }
+
+  test("Api.CarsLive.GetAvailableCars - request") {
+    val request = SttpClientInterpreter.toRequest(Api.CarsLive.GetAvailableCars, uri)
+    val prepared = request(params)
+    assertEquals(prepared.uri.toString, "test.lt/api/CarsLive/GetAvailableCars")
+    assertEquals(prepared.headers, expectedHeaders)
+  }
+
+  test("Api.CarsLive.GetAvailableCars - response") {
+    val testingBackend = SttpBackendStub.synchronous
+      .whenRequestMatches(_ => true)
+      .thenRespond(Files.readString(Paths.get(getClass.getResource("/cars.json").toURI())))
+    val request = SttpClientInterpreter.toRequest(Api.CarsLive.GetAvailableCars, uri)
+    val prepared = request(params)
+    val response = prepared.send(testingBackend)
+
+    val cars = response.body.orThrow
+    assertEquals(
+      cars,
+      List(
+        Car(
+          1369,
+          11,
+          BigDecimal("1.1"),
+          BigDecimal("2.2"),
+          BigDecimal(0.27),
+          false,
+          true,
+          68,
+          "address1",
+          None
+        ),
+        Car(
+          1370,
+          11,
+          BigDecimal("3.3"),
+          BigDecimal("4.4"),
+          BigDecimal("0.27"),
+          false,
+          true,
+          52,
+          "address2",
+          Some("city2")
+        )
+      )
+    )
+  }
+
+}
--- build.sbt
+++ build.sbt
@@ -1,26 +1,60 @@
-organization := "citywasp"
-name := "citywasp-api"
+val Tapir = "0.18.0-M11"
+val Circe = "0.13.0"
+val Ciris = "2.0.1"
 
-scalaVersion := "2.13.6"
+lazy val api = (project in file("api"))
+  .settings(
+    commonSettings,
+    name := "citywasp-api",
+    libraryDependencies ++= Seq(
+      "com.softwaremill.sttp.tapir" %% "tapir-core"           % Tapir,
+      "com.softwaremill.sttp.tapir" %% "tapir-json-circe"     % Tapir,
+      "com.softwaremill.sttp.tapir" %% "tapir-sttp-client"    % Tapir,
+      "com.softwaremill.sttp.tapir" %% "tapir-refined"        % Tapir,
+      "io.circe"                    %% "circe-generic"        % Circe,
+      "io.circe"                    %% "circe-generic-extras" % Circe,
+      "org.scalameta"               %% "munit"                % "0.7.26" % Test
+    )
+  )
+  .enablePlugins(AutomateHeaderPlugin)
 
-val Akka = "2.6.15"
-val AkkaHttp = "10.2.4"
+lazy val cli = (project in file("cli"))
+  .settings(
+    commonSettings,
+    name := "citywasp-cli",
+    libraryDependencies ++= Seq(
+      "is.cir"                        %% "ciris"               % Ciris,
+      "is.cir"                        %% "ciris-refined"       % Ciris,
+      "org.typelevel"                 %% "cats-effect"         % "3.1.1",
+      "com.softwaremill.sttp.client3" %% "http4s-backend"      % "3.3.6",
+      "org.http4s"                    %% "http4s-blaze-client" % "0.23.0-RC1"
+    )
+  )
+  .dependsOn(api)
+  .enablePlugins(AutomateHeaderPlugin, JavaAppPackaging)
 
-libraryDependencies ++= Seq(
-  "com.typesafe.akka" %% "akka-stream"     % Akka,
-  "com.typesafe.akka" %% "akka-http"       % AkkaHttp,
-  "de.heikoseeberger" %% "akka-http-circe" % "1.37.0",
-  "org.scalatest"     %% "scalatest"       % "3.2.9" % "test"
+lazy val commonSettings = Seq(
+  organization := "lt.dvim.citywasp",
+  scalaVersion := "2.13.6",
+  scalacOptions += "-Ymacro-annotations",
+  scalafmtOnCompile := true,
+  scalafixOnCompile := true,
+  startYear := Some(2015),
+  organizationName := "github.com/2m/citywasp-api/graphs/contributors",
+  licenses += ("Apache-2.0", new URL("https://www.apache.org/licenses/LICENSE-2.0.txt")),
+  homepage := Some(url("https://github.com/2m/citywasp-api")),
+  developers := List(
+    Developer(
+      "contributors",
+      "Contributors",
+      "https://gitter.im/2m/general",
+      url("https://github.com/2m/citywasp-api/graphs/contributors")
+    )
+  ),
+  sonatypeProfileName := "lt.dvim",
+  versionScheme := Some("semver-spec")
 )
-
-scalafmtOnCompile := true
-scalafixOnCompile := true
 
 ThisBuild / scalafixDependencies ++= Seq(
   "com.nequissimus" %% "sort-imports" % "0.5.5"
 )
-
-enablePlugins(AutomateHeaderPlugin)
-startYear := Some(2015)
-organizationName := "github.com/2m/citywasp-api/contributors"
-licenses += ("Apache-2.0", new URL("https://www.apache.org/licenses/LICENSE-2.0.txt"))
